cfbfe4c5313806818987c8fd50900e3b1e677f07
jlizancandela <141518966+jlizancandela@users.noreply.github.com>
2024-01-24 12:34:23 +0100

    actualizacion

--- www/wordpress/wp-content/plugins/post-grid/build/index.asset.php
+++ www/wordpress/wp-content/plugins/post-grid/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'ec1a04b3604847a560f3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '9104b48fce001bacfd6f');
